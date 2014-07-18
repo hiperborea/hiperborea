2a12c4179e021595b5d5064fcce5f0719890e859
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-07-18 01:59:45 -0400

    added hash navigation

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,14 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-skrollr.init({
-    forceHeight: true
+var s=skrollr.init({forceHeight: true});
+
+skrollr.menu.init(s,{
+    animate:true,
+    easing:'sqrt',
+    scale:2,
+    duration:function(currentTop,targetTop){
+        return Math.abs(currentTop-targetTop)*5;
+    }
 });
 
