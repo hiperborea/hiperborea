d25f6147d94a1ca6b8e03843f23ffa20aa83f20d
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-02-12 16:25:46 -0400

    working on parallax for site

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,15 @@
-var App=window.App=Ember.Application.create();
+'use strict';
 
-//App.Store=DS.Store.extend();
-App.Skrollr=skrollr.init({forceHeight:false});
+var s=skrollr.init({
+    forceHeight:false
+});
 
-/*skrollr.menu.init(s,{
+skrollr.menu.init(s,{
     animate:true,
     easing:'sqrt',
     scale:2,
     duration:function(currentTop,targetTop){
         return Math.abs(currentTop-targetTop)*5;
     }
-});*/
-
-App.Router.map(function(){
-    this.route('index',{path:'/'});
-    this.route('security');
-    this.route('development');
-    this.route('sysadmin');
 });
-
-App.SkrollrView=Em.View.extend({
-    didInsertElement:function(){
-        App.Skrollr.refresh();
-    }
-});
-
-App.SecurityView=App.SkrollrView.extend();
-App.DevelopmentView=App.SkrollrView.extend();
-App.SysadminView=App.SkrollrView.extend();
 
